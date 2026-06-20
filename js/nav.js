ecc10ad74a4f4cb77ad5de8cb4c1b16914a9a6ee
Eriver111 <eriver111@users.noreply.github.com>
2026-06-20 19:16:08 +0800

    feat: pricing page + global nav bar (top desktop, bottom mobile)

---
+++ js/nav.js
@@ -0,0 +1,20 @@
+(function(){
+  var navHTML = `
+<div id="zhishi-nav" class="zhishi-nav">
+  <a href="/" class="nav-brand">知时</a>
+  <div class="nav-links">
+    <a href="/">首页</a>
+    <a href="/paipan">排盘</a>
+    <a href="/hepan">合盘</a>
+    <a href="/pricing" class="nav-cta">💰 付费</a>
+  </div>
+</div>
+<div id="zhishi-mobile-nav" class="zhishi-mobile-nav">
+  <a href="/">🏠</a>
+  <a href="/paipan">📜</a>
+  <a href="/hepan">💞</a>
+  <a href="/pricing">💰</a>
+</div>
+`;
+  document.body.insertAdjacentHTML('afterbegin', navHTML);
+})();
