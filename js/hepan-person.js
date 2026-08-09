684091e3f770f979d7b3b5ee0b1dc5c9d005456b
Eriver111 <eriver111@users.noreply.github.com>
2026-08-09 20:25:53 +0800

    fix: dist > city priority for true solar time location in result.js and hepan-person.js

--- js/hepan-person.js
+++ js/hepan-person.js
@@ -53,7 +53,7 @@
     var calculated = calculator.calculateFromBirthInput({
       year: params.year, month: params.month, day: params.day, hour: params.hour,
       clock: params.clock, minute: params.minute, gender: params.gender,
-      location: params.city || params.dist || params.prov || '',
+      location: params.dist || params.city || params.prov || '',
       trueSolarTime: params.trueSolarTime,
       ziHourNextDay: params.ziHourNextDay,
     });
